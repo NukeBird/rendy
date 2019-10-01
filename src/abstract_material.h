a43f67ef15b032da8b51bc5aee214931659f9766
Nuke Bird <nukebird.dev@gmail.com>
2019-10-01 19:10:20 +0300

    Add AbstractMaterial::get_flags() pure virtual method

--- src/abstract_material.h
+++ src/abstract_material.h
@@ -4,6 +4,7 @@
 struct AbstractMaterial: public AbstractResource
 {
 	virtual ~AbstractMaterial() = default; //TODO: = 0?
+	virtual uint32_t get_flags() const = 0;
 	virtual void bind() = 0;
 	virtual void unbind() = 0;
 };
