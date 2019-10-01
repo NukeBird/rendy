9fd3dccd7f851ee3327db5aef39d3bac259fdb79
Nuke Bird <nukebird.dev@gmail.com>
2019-10-01 19:50:07 +0300

    Pass extra flags to AbstractMaterial::unbind()

--- src/abstract_material.h
+++ src/abstract_material.h
@@ -7,7 +7,7 @@ struct AbstractMaterial: public AbstractResource
 	virtual ~AbstractMaterial() = default; //TODO: = 0?
 	virtual uint32_t get_flags() const = 0;
 	virtual void bind(uint32_t extra_flags) = 0;
-	virtual void unbind() = 0;
+	virtual void unbind(uint32_t extra_flags) = 0;
 };
 
 using AbstractMaterialRef = std::shared_ptr<AbstractMaterial>;
