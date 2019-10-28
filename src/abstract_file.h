22bed66a10600da7a79245402474521abfa9c8c9
Nuke Bird <nukebird.dev@gmail.com>
2019-10-28 13:15:58 +0300

    Add AbstractFile

---
+++ src/abstract_file.h
@@ -0,0 +1,31 @@
+#pragma once
+#include <cstdint>
+#include <memory>
+#include <string>
+
+enum class FileMode
+{
+	Read,
+	Write,
+	ReadWrite
+};
+
+struct AbstractFile 
+{
+	enum SeekOrigin
+	{
+		Begin,
+		End,
+		Set
+	};
+
+	virtual uint64_t get_size() const = 0;
+	virtual bool is_read_only() const = 0;
+	virtual bool validate() = 0;
+	virtual uint64_t seek(uint64_t offset, SeekOrigin origin = Begin) = 0;
+	virtual uint64_t tell(uint64_t offset) = 0;
+	virtual uint64_t read(void* buffer, uint64_t size) = 0;
+	virtual uint64_t write(void* buffer, uint64_t size) = 0;
+};
+
+using FileRef = std::shared_ptr<AbstractFile>;
