8f57496f4119e04b86ac7db59f59b0966cddb92f
Nuke Bird <nukebird.dev@gmail.com>
2019-11-25 14:13:26 +0300

    Add es2/common.h

---
+++ src/es2/common.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "buffer.h"
+#include "sampler2d.h"
+#include "shader.h"
+#include "shader_variant.h"
+#include "texture_cube.h"
+#include "texture2d.h"
+#include "vertex_array.h"
