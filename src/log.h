bc781071fd1ff9ab909e82001ad5519c8402c225
Nuke Bird <nukebird.dev@gmail.com>
2019-11-27 15:05:46 +0300

    Add OPTICK_EVENTs to logger functions

--- src/log.h
+++ src/log.h
@@ -2,6 +2,7 @@
 #include <spdlog/spdlog.h>
 #include <spdlog/fmt/ostr.h>
 #include <memory>
+#include <optick.h>
 
 namespace Rendy
 {
@@ -12,37 +13,43 @@ namespace Rendy
 
 		template<class T>
 		void info(const T& msg)
-		{
+		{	
+			OPTICK_EVENT();
 			get_logger()->info(msg);
 		}
 
 		template<typename... Args>
 		void info(spdlog::string_view_t fmt, const Args&... args)
 		{
+			OPTICK_EVENT();
 			get_logger()->info(fmt, args...);
 		}
 
 		template<class T>
 		void warn(const T& msg)
 		{
+			OPTICK_EVENT();
 			get_logger()->warn(msg);
 		}
 
 		template<typename... Args>
 		void warn(spdlog::string_view_t fmt, const Args&... args)
 		{
+			OPTICK_EVENT();
 			get_logger()->warn(fmt, args...);
 		}
 
 		template<class T>
 		void error(const T& msg)
 		{
+			OPTICK_EVENT();
 			get_logger()->error(msg);
 		}
 
 		template<typename... Args>
 		void error(spdlog::string_view_t fmt, const Args&... args)
 		{
+			OPTICK_EVENT();
 			get_logger()->error(fmt, args...);
 		}
 	};
