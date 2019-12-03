b9b35530d99e895e77a7a11c573326262228da69
Nuke Bird <nukebird.dev@gmail.com>
2019-12-03 13:22:48 +0300

    Add OPTICK_EVENTs to BindTextureCube

--- src/command/bind_texture_cube.cpp
+++ src/command/bind_texture_cube.cpp
@@ -1,13 +1,18 @@
 #include "bind_texture_cube.h"
+#include <optick.h>
 
 Rendy::BindTextureCube::BindTextureCube(AbstractTextureCubeRef texture, uint32_t slot)
 {
+	OPTICK_EVENT();
+
 	this->texture = texture;
 	this->slot = slot;
 }
 
 void Rendy::BindTextureCube::execute()
 {
+	OPTICK_EVENT();
+
 	if (texture)
 	{
 		texture->bind(slot);
