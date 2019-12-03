ea1e62e3715dbd495fecd59881c87087b778080d
Nuke Bird <nukebird.dev@gmail.com>
2019-12-03 13:19:32 +0300

    Add OPTICK_EVENTs to BufferFactory

--- src/buffer/buffer_factory.cpp
+++ src/buffer/buffer_factory.cpp
@@ -1,15 +1,19 @@
 #include "buffer_factory.h"
 #include "buffer_es2.h"
 #include "buffer_es3.h"
+#include <optick.h>
 
 Rendy::BufferFactory::BufferFactory(OGL version)
 {
+	OPTICK_EVENT();
 	this->version = version;
 }
 
 Rendy::AbstractBufferRef Rendy::BufferFactory::make(BufferTarget target, 
 	uint32_t size, const void* ptr)
 {
+	OPTICK_EVENT();
+
 	switch (version)
 	{
 		case Rendy::OGL::ES20:
