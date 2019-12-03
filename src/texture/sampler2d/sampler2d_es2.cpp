ed5689c5689dd457807aa80d3da361e8186f80c8
Nuke Bird <nukebird.dev@gmail.com>
2019-12-03 13:37:13 +0300

    Add OPTICK_EVENTs to ES2::Sampler2D

--- src/texture/sampler2d/sampler2d_es2.cpp
+++ src/texture/sampler2d/sampler2d_es2.cpp
@@ -1,17 +1,22 @@
 #include "sampler2d_es2.h"
+#include <optick.h>
 
 void Rendy::ES2::Sampler2D::reload()
 {
 	//do nothing
+	OPTICK_EVENT();
 }
 
 bool Rendy::ES2::Sampler2D::validate() const
 {
+	OPTICK_EVENT();
 	return true; //?
 }
 
 void Rendy::ES2::Sampler2D::bind(uint32_t slot)
 {
+	OPTICK_EVENT();
+
 	auto gl_wrap_mode = parse_wrap_mode(wrap_mode);
 	auto gl_min_filter = parse_min_filter(min_filter);
 	auto gl_mag_filter = parse_mag_filter(mag_filter);
@@ -25,40 +30,49 @@ void Rendy::ES2::Sampler2D::bind(uint32_t slot)
 void Rendy::ES2::Sampler2D::unbind(uint32_t slot)
 {
 	//do nothing
+	OPTICK_EVENT();
 }
 
 void Rendy::ES2::Sampler2D::set_min_filter(MinFilter filter)
 {
+	OPTICK_EVENT();
 	min_filter = filter;
 }
 
 void Rendy::ES2::Sampler2D::set_mag_filter(MagFilter filter)
 {
+	OPTICK_EVENT();
 	mag_filter = filter;
 }
 
 void Rendy::ES2::Sampler2D::set_wrap_mode(WrapMode mode)
 {
+	OPTICK_EVENT();
 	wrap_mode = mode;
 }
 
 Rendy::MinFilter Rendy::ES2::Sampler2D::get_min_filter() const
 {
+	OPTICK_EVENT();
 	return min_filter;
 }
 
 Rendy::MagFilter Rendy::ES2::Sampler2D::get_mag_filter() const
 {
+	OPTICK_EVENT();
 	return mag_filter;
 }
 
 Rendy::WrapMode Rendy::ES2::Sampler2D::get_wrap_mode() const
 {
+	OPTICK_EVENT();
 	return wrap_mode;
 }
 
 GLenum Rendy::ES2::Sampler2D::parse_min_filter(MinFilter filter) const
 {
+	OPTICK_EVENT();
+
 	switch (filter)
 	{
 		case Rendy::MinFilter::Nearest:
@@ -92,6 +106,8 @@ GLenum Rendy::ES2::Sampler2D::parse_min_filter(MinFilter filter) const
 
 GLenum Rendy::ES2::Sampler2D::parse_mag_filter(MagFilter filter) const
 {
+	OPTICK_EVENT();
+
 	switch (filter)
 	{
 		case Rendy::MagFilter::Nearest:
@@ -109,6 +125,8 @@ GLenum Rendy::ES2::Sampler2D::parse_mag_filter(MagFilter filter) const
 
 GLenum Rendy::ES2::Sampler2D::parse_wrap_mode(WrapMode mode) const
 {
+	OPTICK_EVENT();
+
 	switch (mode)
 	{
 		case Rendy::WrapMode::ClampToEdge:
