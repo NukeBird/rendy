39cb5e0acf27bceb2d3e36ad1bb37e89775889d7
Nuke Bird <nukebird.dev@gmail.com>
2019-12-03 13:50:03 +0300

    Add OPTICK_EVENTs to GAPI

--- src/gapi.cpp
+++ src/gapi.cpp
@@ -1,7 +1,10 @@
 #include "gapi.h"
+#include <optick.h>
 
 Rendy::GAPI::GAPI(OGL version)
 {
+	OPTICK_EVENT();
+
 	this->version = version;
 	buffer_factory = std::make_shared<BufferFactory>(version);
 	shader_factory = std::make_shared<ShaderFactory>(version);
@@ -13,63 +16,76 @@ Rendy::GAPI::GAPI(OGL version)
 Rendy::AbstractShaderRef Rendy::GAPI::make_shader(const std::string& vtx,
 	const std::string& frg)
 {
+	OPTICK_EVENT();
 	return shader_factory->make(vtx, frg);
 }
 
 Rendy::AbstractTexture2DRef Rendy::GAPI::make_texture2d(Image2DRef image)
 {
+	OPTICK_EVENT();
 	return tex2d_factory->make(image);
 }
 
 Rendy::AbstractTextureCubeRef Rendy::GAPI::make_texture_cube(uint32_t size,
 	const void* ptr)
 {
+	OPTICK_EVENT();
 	return tex_cube_factory->make(ptr, size);
 }
 
 Rendy::AbstractVertexArrayRef Rendy::GAPI::make_vao(AbstractBufferRef vbo,
 	AbstractBufferRef ibo, BufferLayoutRef layout)
 {
+	OPTICK_EVENT();
 	return vao_factory->make(vbo, ibo, layout);
 }
 
 Rendy::AbstractBufferRef Rendy::GAPI::make_vbo(uint32_t size, const void* ptr)
 {
+	OPTICK_EVENT();
 	return buffer_factory->make(BufferTarget::VBO, size, ptr);
 }
 
 Rendy::AbstractBufferRef Rendy::GAPI::make_ibo(uint32_t size, const void* ptr)
 {
+	OPTICK_EVENT();
 	return buffer_factory->make(BufferTarget::IBO, size, ptr);
 }
 
 Rendy::BufferFactoryRef Rendy::GAPI::get_buffer_factory() const
 {
+	OPTICK_EVENT();
 	return buffer_factory;
 }
 
 Rendy::ShaderFactoryRef Rendy::GAPI::get_shader_factory() const
 {
+	OPTICK_EVENT();
 	return shader_factory;
 }
 
 Rendy::Texture2DFactoryRef Rendy::GAPI::get_texture2d_factory() const
 {
+	OPTICK_EVENT();
 	return tex2d_factory;
 }
 
 Rendy::TextureCubeFactoryRef Rendy::GAPI::get_texture_cube_factory() const
 {
+	OPTICK_EVENT();
 	return tex_cube_factory;
 }
 
 Rendy::VertexArrayFactoryRef Rendy::GAPI::get_vertex_array_factory() const
 {
+	OPTICK_EVENT();
 	return vao_factory;
 }
 
 Rendy::IndexType Rendy::GAPI::get_index_type() const
 {
+	OPTICK_EVENT();
+
 	switch (version)
 	{
 		case Rendy::OGL::ES20:
