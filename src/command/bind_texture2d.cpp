175b3b24227185882942871ccae95c49b3826ea5
Nuke Bird <nukebird.dev@gmail.com>
2019-12-03 13:23:22 +0300

    Add OPTICK_EVENTs to BindTexture2D

--- src/command/bind_texture2d.cpp
+++ src/command/bind_texture2d.cpp
@@ -1,13 +1,18 @@
 #include "bind_texture2d.h"
+#include <optick.h>
 
 Rendy::BindTexture2D::BindTexture2D(AbstractTexture2DRef texture, uint32_t slot)
 {
+	OPTICK_EVENT();
+
 	this->texture = texture;
 	this->slot = slot;
 }
 
 void Rendy::BindTexture2D::execute()
 {
+	OPTICK_EVENT();
+
 	if (texture)
 	{
 		texture->bind(slot); //TODO: possibly invalid slot?
