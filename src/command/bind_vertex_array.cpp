acd8e2f8d65ccecdefcec6e789f8c0f198ce3693
Nuke Bird <nukebird.dev@gmail.com>
2019-12-03 13:24:08 +0300

    Add OPTICK_EVENTs to BindVertexArray

--- src/command/bind_vertex_array.cpp
+++ src/command/bind_vertex_array.cpp
@@ -1,13 +1,18 @@
 #include "bind_vertex_array.h"
+#include <optick.h>
 
 Rendy::BindVertexArray::BindVertexArray(AbstractVertexArrayRef vao, ShaderVariantRef shader_variant)
 {
+	OPTICK_EVENT();
+
 	this->vao = vao;
 	this->shader_variant = shader_variant;
 }
 
 void Rendy::BindVertexArray::execute()
 {
+	OPTICK_EVENT();
+
 	if (vao && shader_variant)
 	{
 		vao->bind(shader_variant);
