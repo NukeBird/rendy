393deda48e58517838ccefe54b4448bb93a7fa8a
Nuke Bird <nukebird.dev@gmail.com>
2019-12-03 13:40:26 +0300

    Add OPTICK_EVENTs to TextureCubeFactory

--- src/texture/texture_cube/texture_cube_factory.cpp
+++ src/texture/texture_cube/texture_cube_factory.cpp
@@ -1,14 +1,18 @@
 #include "texture_cube_factory.h"
 #include "texture_cube_es2.h"
 #include "texture_cube_es3.h"
+#include <optick.h>
 
 Rendy::TextureCubeFactory::TextureCubeFactory(OGL version)
 {
+	OPTICK_EVENT();
 	this->version = version;
 }
 
 Rendy::AbstractTextureCubeRef Rendy::TextureCubeFactory::make(const std::string& filename)
 {
+	OPTICK_EVENT();
+
 	switch (version)
 	{
 		case Rendy::OGL::ES20:
@@ -28,6 +32,8 @@ Rendy::AbstractTextureCubeRef Rendy::TextureCubeFactory::make(const std::string&
 Rendy::AbstractTextureCubeRef Rendy::TextureCubeFactory::make(const void* memory, 
 	uint32_t size)
 {
+	OPTICK_EVENT();
+
 	switch (version)
 	{
 		case Rendy::OGL::ES20:
