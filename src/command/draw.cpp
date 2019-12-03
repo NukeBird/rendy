2fee099df10df0b66c39e26314387cd91e5b1238
Nuke Bird <nukebird.dev@gmail.com>
2019-12-03 13:24:39 +0300

    Add OPTICK_EVENTs to Draw

--- src/command/draw.cpp
+++ src/command/draw.cpp
@@ -1,12 +1,16 @@
 #include "draw.h"
+#include <optick.h>
 
 Rendy::Draw::Draw(AbstractVertexArrayRef vao)
 {
+	OPTICK_EVENT();
 	this->vao = vao;
 }
 
 void Rendy::Draw::execute()
 {
+	OPTICK_EVENT();
+
 	if (vao)
 	{
 		vao->draw();
