01c9a65947fb0db5e45017c5a22aafb498d948ac
Nuke Bird <nukebird.dev@gmail.com>
2019-12-03 13:49:08 +0300

    Add OPTICK_EVENTs to AbstractRenderPass

--- src/abstract_render_pass.cpp
+++ src/abstract_render_pass.cpp
@@ -1,11 +1,14 @@
 #include "abstract_render_pass.h"
+#include <optick.h>
 
 void Rendy::AbstractRenderPass::reload()
 {
 	//DO NOTHING
+	OPTICK_EVENT();
 }
 
 bool Rendy::AbstractRenderPass::validate() const
 {
+	OPTICK_EVENT();
 	return true;
 }
