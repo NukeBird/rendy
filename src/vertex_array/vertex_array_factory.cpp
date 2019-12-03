23a6d51bd705788473ab198bd0c45399ae2237cb
Nuke Bird <nukebird.dev@gmail.com>
2019-12-03 13:47:57 +0300

    Add OPTICK_EVENTs to VertexArrayFactory

--- src/vertex_array/vertex_array_factory.cpp
+++ src/vertex_array/vertex_array_factory.cpp
@@ -1,15 +1,19 @@
 #include "vertex_array_factory.h"
 #include "vertex_array_es2.h"
 #include "vertex_array_es3.h"
+#include <optick.h>
 
 Rendy::VertexArrayFactory::VertexArrayFactory(OGL version)
 {
+	OPTICK_EVENT();
 	this->version = version;
 }
 
 Rendy::AbstractVertexArrayRef Rendy::VertexArrayFactory::make(AbstractBufferRef vbo,
 	AbstractBufferRef ibo, const BufferLayoutRef& layout)
 {
+	OPTICK_EVENT();
+
 	switch (version)
 	{
 		case Rendy::OGL::ES20:
