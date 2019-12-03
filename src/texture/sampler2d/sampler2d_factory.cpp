448e50b114c07d0a970b900b2fd842769272e754
Nuke Bird <nukebird.dev@gmail.com>
2019-12-03 13:37:34 +0300

    Add OPTICK_EVENTs to SamplerFactory

--- src/texture/sampler2d/sampler2d_factory.cpp
+++ src/texture/sampler2d/sampler2d_factory.cpp
@@ -1,14 +1,18 @@
 #include "sampler2d_factory.h"
 #include "sampler2d_es2.h"
 #include "sampler2d_es3.h"
+#include <optick.h>
 
 Rendy::Sampler2DFactory::Sampler2DFactory(OGL version)
 {
+	OPTICK_EVENT();
 	this->version = version;
 }
 
 Rendy::AbstractSampler2DRef Rendy::Sampler2DFactory::make()
 {
+	OPTICK_EVENT();
+
 	switch (version)
 	{
 		case Rendy::OGL::ES20:
