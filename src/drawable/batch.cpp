9d2a2e767a257c69cce9a280d01ab6a80f6d0ee0
Nuke Bird <nukebird.dev@gmail.com>
2019-12-03 13:26:56 +0300

    Add OPTICK_EVENTs to Batch

--- src/drawable/batch.cpp
+++ src/drawable/batch.cpp
@@ -4,9 +4,12 @@
 #include "../command/bind_vertex_array.h"
 #include "../command/draw.h"
 #include <memory>
+#include <optick.h>
 
 Rendy::CommandList Rendy::Batch::to_command_list() const
 {
+	OPTICK_EVENT();
+
 	auto list = material->to_command_list(extra_flags);
 
 	auto shader_variant = material->get_shader_variant(extra_flags);
