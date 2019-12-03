5348981c2b79babf22a828d244f11b7ba09964f9
Nuke Bird <nukebird.dev@gmail.com>
2019-12-03 13:48:38 +0300

    Add OPTICK_EVENTs to AbstractEngine

--- src/abstract_engine.cpp
+++ src/abstract_engine.cpp
@@ -76,35 +76,42 @@ void Rendy::AbstractEngine::add_stage(AbstractRenderPassRef stage)
 
 Rendy::AbstractShaderRef Rendy::AbstractEngine::make_shader(const std::string & vtx, const std::string & frg)
 {
+	OPTICK_EVENT();
 	return get_gapi()->make_shader(vtx, frg);
 }
 
 Rendy::AbstractTexture2DRef Rendy::AbstractEngine::make_texture2d(Image2DRef image)
 {
+	OPTICK_EVENT();
 	return get_gapi()->make_texture2d(image);
 }
 
 Rendy::AbstractTextureCubeRef Rendy::AbstractEngine::make_texture_cube(uint32_t size, const void * ptr)
 {
+	OPTICK_EVENT();
 	return get_gapi()->make_texture_cube(size, ptr);
 }
 
 Rendy::AbstractVertexArrayRef Rendy::AbstractEngine::make_vao(AbstractBufferRef vbo, AbstractBufferRef ibo, BufferLayoutRef layout)
 {
+	OPTICK_EVENT();
 	return get_gapi()->make_vao(vbo, ibo, layout);
 }
 
 Rendy::AbstractBufferRef Rendy::AbstractEngine::make_vbo(uint32_t size, const void * ptr)
 {
+	OPTICK_EVENT();
 	return get_gapi()->make_vbo(size, ptr);
 }
 
 Rendy::AbstractBufferRef Rendy::AbstractEngine::make_ibo(uint32_t size, const void* ptr)
 {
+	OPTICK_EVENT();
 	return get_gapi()->make_ibo(size, ptr);
 }
 
 Rendy::IndexType Rendy::AbstractEngine::get_index_type() const
 {
+	OPTICK_EVENT();
 	return get_gapi()->get_index_type();
 }
