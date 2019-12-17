b8a7e545377494d0518255d6d297f38f54abe8b5
Nuke Bird <nukebird.dev@gmail.com>
2019-12-17 15:19:36 +0300

    Add OPTICK_EVENTs to ShaderSource

--- src/shader/shader_source.cpp
+++ src/shader/shader_source.cpp
@@ -1,30 +1,38 @@
 #include "shader_source.h"
+#include <optick.h>
 #include <cassert>
 
 Rendy::ShaderSource::ShaderSource(const std::string& vtx, const std::string& frg)
 {
+	OPTICK_EVENT();
+
 	set_source(ShaderType::VertexShader, vtx);
 	set_source(ShaderType::FragmentShader, frg);
 }
 
 void Rendy::ShaderSource::set_source(ShaderType type, const std::string& source)
 {
+	OPTICK_EVENT();
 	sources[type] = source;
 }
 
 const std::string& Rendy::ShaderSource::get_source(ShaderType type)
 {
+	OPTICK_EVENT();
 	return sources[type];
 }
 
 Rendy::ShaderSourceRef Rendy::ShaderSource::combine(ShaderSourceRef source)
 {
+	OPTICK_EVENT();
 	assert(source != nullptr);
 	return combine(*source);
 }
 
 Rendy::ShaderSourceRef Rendy::ShaderSource::combine(ShaderSource& source)
 {
+	OPTICK_EVENT();
+
 	auto result = std::make_shared<ShaderSource>();
 
 	result->sources[ShaderType::VertexShader] = sources[ShaderType::VertexShader] +
@@ -38,6 +46,8 @@ Rendy::ShaderSourceRef Rendy::ShaderSource::combine(ShaderSource& source)
 
 bool Rendy::ShaderSource::operator==(ShaderSource& s)
 {
+	OPTICK_EVENT();
+
 	if (get_source(ShaderType::VertexShader) != s.get_source(ShaderType::VertexShader))
 	{
 		return false;
