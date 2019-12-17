7fc8555fe2f5a5a0fcdda8f84eaf1c9ac4128607
Nuke Bird <nukebird.dev@gmail.com>
2019-12-17 12:48:05 +0300

    Add ShaderSource

---
+++ src/shader/shader_source.cpp
@@ -0,0 +1,26 @@
+#include "shader_source.h"
+#include <cassert>
+
+void Rendy::ShaderSource::set_source(ShaderType type, const std::string& source)
+{
+	sources[type] = source;
+}
+
+Rendy::ShaderSourceRef Rendy::ShaderSource::combine(ShaderSourceRef source)
+{
+	assert(source != nullptr);
+	return combine(*source);
+}
+
+Rendy::ShaderSourceRef Rendy::ShaderSource::combine(ShaderSource& source)
+{
+	auto result = std::make_shared<ShaderSource>();
+
+	result->sources[ShaderType::VertexShader] = sources[ShaderType::VertexShader] +
+		source.sources[ShaderType::VertexShader];
+
+	result->sources[ShaderType::FragmentShader] = sources[ShaderType::FragmentShader] +
+		source.sources[ShaderType::FragmentShader];
+
+	return result;
+}
---
+++ src/shader/shader_source.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "shader_type.h"
+#include <unordered_map>
+#include <vector>
+#include <string>
+#include <memory>
+
+namespace Rendy
+{
+	class ShaderSource;
+
+	using ShaderSourceRef = std::shared_ptr<ShaderSource>;
+
+	class ShaderSource final
+	{
+	public:
+		void set_source(ShaderType type, const std::string& source);
+		ShaderSourceRef combine(ShaderSourceRef source);
+		ShaderSourceRef combine(ShaderSource& source);
+	private:
+		std::unordered_map<ShaderType, std::string> sources;
+	};
+};
